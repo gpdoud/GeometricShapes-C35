f909ff433c850d8aa829bad95e6472868d623457
Greg Doud <gpdoud@gmail.com>
2022-02-04 12:40:21 -0500

    Add project files.

---
+++ GeometricShapes/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GeometricShapes {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Rectangle rect = new Rectangle(2, 5);
+            rect.PrintPerimeter();
+        }
+    }
+}
---
+++ GeometricShapes/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes {
+    public class Rectangle {
+
+        public int Height { get; set;  }
+        public int Width { get; set; }
+
+        public Rectangle(int height, int width) {
+            Height = height;
+            Width = width;
+        }
+
+        public void PrintPerimeter() {
+            int perimeter = (Height + Width) * 2;
+            Console.WriteLine($"A rectange with a height of {Height} and width of {Width} " +
+                                $"has a perimeter of {perimeter}");
+        }
+
+    }
+}
